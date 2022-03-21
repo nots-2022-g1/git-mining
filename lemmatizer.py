1163c48d6cc5d7de46df5858a21b5bdb18dd0e5b
Yael Bakker <yv.bakker@student.han.nl>
2022-03-21 13:26:42 +0100

    Update experimental pre-processor code

--- lemmatizer.py
+++ lemmatizer.py
@@ -1,33 +1,56 @@
 # import these modules
 import json
+import string
+import pandas as pd
+import regex as re
+
 import nltk
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
+
+# Download required assets (will skip if already downloaded)
+nltk.download('wordnet')
+nltk.download('omw-1.4')
 nltk.download('punkt')
 
+# Load JSON from file
 f = open('angular.json')
 data = json.load(f)
 
 lemmatizer = WordNetLemmatizer()
 
-messages = []
+# Get git commit messages from raw data by querying key 'message' for each entry in JSON data
+messages = [i['message'] for i in data]
 
-for i in data:
-    messages.append(i['message'])
+# Remove punctuation from message strings
+# https://stackoverflow.com/questions/265960/best-way-to-strip-punctuation-from-a-string
+messagesWithoutUrls = [re.sub(r'http\S+', '', m) for m in messages]
+messagesWithoutPunctuation = [m.translate(str.maketrans('', '', string.punctuation)) for m in messagesWithoutUrls]
+messagesWithoutNumPunctuation = [m.translate(str.maketrans('', '', string.digits)) for m in messagesWithoutPunctuation]
+messagesWithoutNumPunctuationNormalized = [m.lower() for m in messagesWithoutNumPunctuation]
 
-tokenizedMessages = []
+# Remove numbers from message strings since we won't be using them for now
+# messagesWithoutPunctuationAndNumbers = ''.join(i for i in messagesWithoutPunctuation if not i.isdigit())
 
-for m in messages:
-    tokenizedMessages.append(word_tokenize(m))
+# Tokenize message strings, word by word
+tokenizedMessages = [word_tokenize(m) for m in messagesWithoutNumPunctuationNormalized]
 
-print(tokenizedMessages[0])
+lemmatizedTokenizedMessages = []
 
-# for word in tokenizedMessages[0]:
-# print(lemmatizer.lemmatize(word))
+for message in tokenizedMessages:
+    lemmatizedTokenizedMessage = [lemmatizer.lemmatize(word) for word in message]
+    lemmatizedTokenizedMessages.append(lemmatizedTokenizedMessage)
+# Lemmatize tokenized message strings
+# lemmatizedMessages = [word for message in tokenizedMessages for word in message]
 
-tokenizedMessages[0] = [lemmatizer.lemmatize(m) for m in tokenizedMessages[0]]
+restoredMessages = []
+for tokenizedMessage in lemmatizedTokenizedMessages:
+    message = ""
+    for token in tokenizedMessage:
+        message += f'{token} '
+    restoredMessages.append(message)
 
+df = pd.DataFrame(messages)
+df.to_csv('angular.original.csv')
 
-print(tokenizedMessages[0])
-
-print("better :", lemmatizer.lemmatize("better", pos="a"))
+# print(restoredMessages[0])
